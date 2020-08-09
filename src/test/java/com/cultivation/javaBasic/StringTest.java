aa06821b512c6163de6447026bc4f1d2caae3a63
lpdft <772346730@qq.com>
2020-08-09 16:54:00 +0800

    [ruyue] Android-StringType:will_create_new_string_when_concat test

--- src/test/java/com/cultivation/javaBasic/StringTest.java
+++ src/test/java/com/cultivation/javaBasic/StringTest.java
@@ -55,7 +55,8 @@ class StringTest {
         //
         // It is really easy to pass the test. But you have to tell why.
         // <--start
-        final Optional<Boolean> areSame = Optional.empty();
+        final Optional<Boolean> areSame = Optional.of(false);
+        //Changed original string
         // --end-->
 
         assertEquals("Part one. Part two.", originalString);
