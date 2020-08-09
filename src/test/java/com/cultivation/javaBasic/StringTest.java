370f2c39af1ed03d1fb1fc17ca58693b52e8efaa
lpdft <772346730@qq.com>
2020-08-09 19:17:58 +0800

    [ruyue] Android-StringType:should_convert_unicode_escape test

--- src/test/java/com/cultivation/javaBasic/StringTest.java
+++ src/test/java/com/cultivation/javaBasic/StringTest.java
@@ -185,7 +185,7 @@ class StringTest {
         // こ - U+3053
         // れ - U+308c
         // <--Start
-        final String actual = null;
+        final String actual = "\u306a\u306b\u3053\u308c";
         // --End-->
 
         assertEquals(expected, actual);
